6e247f6dd2d023872666e05de611be3acf6e2088
mschiddy <mschiddy@gmail.com>
2017-08-10 13:46:54 -0600

    Updated dbpath for Heroku app

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://mdbuser2:todoapp||mongodb://localhost:27017/TodoApp');
+var appUri = "mongodb://mdbuser2:dib450@ds055574.mlab.com:55574/todoapp";
+
+var promise = mongoose.connect(appUri || "mongodb://localhost:27017/TodoApp", {
+	useMongoClient: true,
+});
 
 module.exports = {mongoose};
