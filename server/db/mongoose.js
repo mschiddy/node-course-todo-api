3f600eb65d932ed6056aa190e040c740dd7832bd
mschiddy <mschiddy@gmail.com>
2017-08-21 13:55:24 -0600

    updated db and added remove

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,8 @@
 var mongoose = require('mongoose');
 
-var appUri = "mongodb://mdbuser2:dib450@ds055574.mlab.com:55574/todoapp";
-
-var promise = mongoose.connect(appUri || "mongodb://localhost:27017/TodoApp", {
-	useMongoClient: true,
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp", {
+	useMongoClient: true
 });
 
 module.exports = {mongoose};
